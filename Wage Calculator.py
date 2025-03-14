8b65761050e515e7757c6fd4f37c79dbb4050329
OmkarMakesStuff <contactomkar21@gmail.com>
2025-03-14 11:49:53 +0530

    Utility Projects

--- Wage Calculator.py
+++
@@ -1,8 +0,0 @@
-print('Hi')
-hours = input('Hours Worked: ')
-rate = input('Hourly rate: ')
-hours = int(hours)
-rate = int(rate)
-wage = hours * rate
-print('Daily Wages =',wage)
-print('Monthly Wages =',wage * 30)
